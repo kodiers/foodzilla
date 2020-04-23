816aa91657dfdeebdd5a25125347c806dc8630d2
Victor Yamchinov <kodiers@gmail.com>
2020-04-24 02:47:32 +0300

    Initial Commit

---
+++ foodzilla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodzilla
+//
+//  Created by Viktor Yamchinov on 24.04.2020.
+//  Copyright © 2020 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
