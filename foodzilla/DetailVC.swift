84dcb022d0deb452f2168d7b20acdae7d5320047
Victor Yamchinov <kodiers@gmail.com>
2020-04-28 02:16:12 +0300

    added layout

---
+++ foodzilla/DetailVC.swift
@@ -0,0 +1,46 @@
+//
+//  DetailVC.swift
+//  foodzilla
+//
+//  Created by Viktor Yamchinov on 28.04.2020.
+//  Copyright © 2020 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemNameLbl: UILabel!
+    @IBOutlet weak var itemPriceLbl: UILabel!
+    @IBOutlet weak var uglyAdView: UIView!
+    @IBOutlet weak var buyItemBtn: UIButton!
+    @IBOutlet weak var hideAddBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func buyBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func hideAdsWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ foodzilla/StorefrontVC.swift
@@ -0,0 +1,38 @@
+//
+//  StorefrontVC.swift
+//  foodzilla
+//
+//  Created by Viktor Yamchinov on 24.04.2020.
+//  Copyright © 2020 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class StorefrontVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    @IBAction func restoreBtnWasPressed(_ sender: Any) {
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+}
+
